e508c17c0807b3ba285b980101049f4d594a5df8
Adithyan S Pillai <101512399+XXOriginalXX@users.noreply.github.com>
2025-02-16 07:32:00 +0530

    config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    APYHUB_API_KEY: 'APY0YGvYtAtixseViWFXJ7YSVQKMqNeIiFw4ltoRSx8X78LXWl1EEUG7WjCJhpydnVH',
-    APYHUB_API_URL: 'https://api.apyhub.com/ai/summarize-documents/file'
-  };
-  
-  export default config;
